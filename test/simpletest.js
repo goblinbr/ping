d3177aafcc22f0e008837a4c097df43de76b220c
Rodrigo de Bona Sartor <rodrigo.goblin@gmail.com>
2016-01-28 10:44:41 -0200

    Added a simple test example

---
+++ test/simpletest.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+
+describe('String#split', function(){
+	beforeEach(function(){
+		current = 'a,b,c'.split(',');
+	})
+
+	it('should return an array', function(){
+		assert(Array.isArray(current));
+	});
+
+	it('should return length = 3', function(){
+		assert(current.length == 3);
+	});
+})
